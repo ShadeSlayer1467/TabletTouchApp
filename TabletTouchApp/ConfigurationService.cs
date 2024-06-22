81ff42655a33e153d994d2e0c005719ca6e11b55
Matthew Read <matthew.read.101@gmail.com>
2024-06-21 20:39:06 -0700

    hk

---
+++ TabletTouchApp/ConfigurationService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Configuration;
+using System.IO;
+
+public class ConfigurationService
+{
+    public IConfiguration Configuration { get; }
+
+    public ConfigurationService()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("secrets.json", optional: false, reloadOnChange: true);
+
+        Configuration = builder.Build();
+    }
+
+    public string GetVendorId()
+    {
+        return Configuration["DeviceSettings:VendorId"];
+    }
+
+    public string GetProductId()
+    {
+        return Configuration["DeviceSettings:ProductId"];
+    }
+}
