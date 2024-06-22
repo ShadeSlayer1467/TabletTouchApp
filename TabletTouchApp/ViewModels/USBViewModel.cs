a194c6b1715d4cd3c15ba9bd7a48ad5348881ebf
Matthew Read <matthew.read.101@gmail.com>
2024-06-21 18:28:45 -0700

    Added A button

---
+++ TabletTouchApp/ViewModels/USBViewModel.cs
@@ -0,0 +1,35 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabletTouchApp.ViewModels
+{
+    public class USBViewModel : ObservableObject
+    {
+        private string status;
+
+        public string Status
+        {
+            get => status;
+            set => SetProperty(ref status, value);
+        }
+
+        public Command ConnectCommand { get; }
+
+        public USBViewModel()
+        {
+            ConnectCommand = new Command(ExecuteConnect);
+            Status = "Disconnected";
+        }
+
+        private void ExecuteConnect()
+        {
+            Status = "Connecting...";
+            // Logic to handle USB connection
+        }
+    }
+}
+
